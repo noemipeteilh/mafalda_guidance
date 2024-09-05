375b39de852c2081924369184cc6081c9a40c921
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-09-05 14:14:36 +0200

    Add button

---
+++ src/components/styled-button.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom'; 
+
+
+
+export const But = styled(Link)`
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: ${({ backgroundColor }) => backgroundColor || '#Ff914d'}; 
+color: ${({ textColor }) => textColor || '#fff'}; 
+font-size: 24px !important;
+border: none;
+border-radius: 30px;
+padding: 12px 24px;
+width: 100%;
+max-width: 350px;
+min-width: 200px;
+height: 60px;
+cursor: pointer;
+text-decoration: none;
+transition: background-color 0.3s;
+font-family: "Inter", sans-serif !important;
+margin-bottom: 100px;
+gap:10px;
+
+`;
+
